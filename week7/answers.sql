395520a867d1f4af6d063671f66ea4db18d471d0
AbrahamDG <151337793+AbrahamDG@users.noreply.github.com>
2024-11-26 23:21:32 -0800

    Updated Tables and Created Character items view

--- week7/answers.sql
+++ week7/answers.sql
@@ -14,20 +14,20 @@ CREATE TABLE characters(
   player_id INT UNSIGNED NOT NULL,
   name VARCHAR(30) NOT NULL,
   `level` UNSIGNED TINYINT,
-  CONSTRAINT foreign_key_player FOREIGN KEY (player_id) REFERENCES players(player_id)
+  CONSTRAINT fk_characters_player FOREIGN KEY (player_id) REFERENCES players(player_id)
 );
 
 CREATE TABLE winners(
   character_id INT UNSIGNED NOT NULL,
   name VARCHAR(30) NOT NULL,
-  CONSTRAINT foreign_key_character FOREIGN KEY (character_id) REFERENCES characters(character_id)
+  CONSTRAINT fk_winners_character FOREIGN KEY (character_id) REFERENCES characters(character_id)
 );
 
 CREATE TABLE character_stats(
   character_id INT UNSIGNED NOT NULL,
   health UNSIGNED TINYINT,
   armor UNSIGNED TINYINT,
-  CONSTRAINT foreign_key_stats FOREIGN KEY (character_id) REFERENCES character(character_id)
+  CONSTRAINT fk_character_stats_stats FOREIGN KEY (character_id) REFERENCES characters(character_id)
 );
 
 CREATE TABLE teams(
@@ -39,8 +39,8 @@ CREATE TABLE team_member(
   team_member_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   team_id INT UNSIGNED NOT NULL,
   character_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_teams FOREIGN KEY (team_id) REFERENCES teams(team_id),
-  CONSTRAINT fk_characters FOREIGN KEY (character_id) REFERENCES characters(character_id)
+  CONSTRAINT fk_team_member_teams FOREIGN KEY (team_id) REFERENCES teams(team_id),
+  CONSTRAINT fk_team_member_characters FOREIGN KEY (character_id) REFERENCES characters(character_id)
 );
 
 CREATE TABLE items(
@@ -53,13 +53,28 @@ CREATE TABLE inventory(
   inventory_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   character_id INT UNSIGNED NOT NULL,
   item_id INT UNSIGNED NOT NULL,
-  CONSTRAINT fk_inventory_chcaracters FOREIGN KEY (character_id) REFERENCES characters(character_id),
-  CONSTRAINT fk_items FOREIGN KEY (item_id) REFERENCES items(item_id)
+  CONSTRAINT fk_inventory_characters FOREIGN KEY (character_id) REFERENCES characters(character_id),
+  CONSTRAINT fk_inventory_items FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
 CREATE TABLE equipped(
   equipped_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   character_id INT UNSIGNED NOT NULL,
-  item_id INT UNSIGNED NOT NULL
-  CONSTRAINT fk_eqipped_characters FOREIGN KEY (character_id) REFERENCES characters(character_id),
+  item_id INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_equipped_characters FOREIGN KEY (character_id) REFERENCES characters(character_id),
   CONSTRAINT fk_equipped_items FOREIGN KEY (item_id) REFERENCES items(item_id)
 );
+-- Views -- 
+CREATE VIEW character_items AS 
+SELECT i.item_id,i.name
+FROM items i
+  JOIN(
+    SELECT item_id, character_id
+      FROM inventory
+
+    UNION 
+
+    SELECT item_id, character_id
+    FROM equipped
+  ) AS total_items
+JOIN items i ON i.items_id = total_items.item_id
+GROUP BY i.item_id, i.name;
