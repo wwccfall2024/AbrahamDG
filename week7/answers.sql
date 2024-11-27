710393749c507f0b15ff859f1884552db3625703
AbrahamDG <151337793+AbrahamDG@users.noreply.github.com>
2024-11-26 21:44:45 -0800

    Created Players Table.

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,10 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players (
+  player_id UNSIGNED PRIMARY KEY NO NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARVHAR(50) NOT NULL
+);
